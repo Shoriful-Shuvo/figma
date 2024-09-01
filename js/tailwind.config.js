6424e441ac83ad6443c9d6aac5c7ae0c1b45b181
Shoriful-Shuvo <shuvo696727xyz@gmail.com>
2024-09-01 19:05:40 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,30 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          btncolorh: '#2F93FD',
+          btncolor: '#247FFB',
+          txt: '#001838',
+          border: '#37B865',
+          boack: 'rgba(255, 255, 255, 0.7)',
+        },
+        maxWidth: {
+            container: '1320px',
+          },
+          fontSize: {
+            banner_h: '64px',
+            icon: '30px',
+            banner_p: '20px',
+            box_p: '24px',
+          },
+          fontFamily: {
+            paprika: ['Paprika'],
+            open: ['Open Sans'],
+          },
+          backgroundImage: {
+            'background': "url('./images/random.png')",
+            
+          },
+      }
+    }
+  }
